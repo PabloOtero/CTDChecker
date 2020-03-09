2746f531d00040ed1b01350af3d87e94b5678420
Pablo Otero <otero_pablo@hotmail.com>
2020-03-09 08:45:50 +0100

    Add files via upload

---
+++ extras.py
@@ -0,0 +1,330 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.ma as ma
+from pandas import Series
+from math import sin, cos, sqrt, atan2, radians
+
+
+def _extrap1d(interpolator):
+    """
+    http://stackoverflow.com/questions/2745329/
+    How to make scipy.interpolate return an extrapolated result beyond the
+    input range.
+
+    """
+    xs, ys = interpolator.x, interpolator.y
+
+    def pointwise(x):
+        if x < xs[0]:
+            return ys[0] + (x - xs[0]) * (ys[1] - ys[0]) / (xs[1] - xs[0])
+        elif x > xs[-1]:
+            return ys[-1] + (x - xs[-1]) * (ys[-1] - ys[-2]) / (xs[-1] - xs[-2])
+        else:
+            return interpolator(x)
+
+    def ufunclike(xs):
+        return np.array(list(map(pointwise, np.array(xs))))
+
+    return ufunclike
+
+
+def get_maxdepth(self):
+    valid_last_depth = self.apply(Series.notnull).values.T
+    return np.float_(self.index.values * valid_last_depth).max(axis=1)
+
+
+def extrap_sec(data, dist, depth, w1=1.0, w2=0):
+    """
+    Extrapolates `data` to zones where the shallow stations are shadowed by
+    the deep stations.  The shadow region usually cannot be extrapolates via
+    linear interpolation.
+
+    The extrapolation is applied using the gradients of the `data` at a certain
+    level.
+
+    Parameters
+    ----------
+    data : array_like
+          Data to be extrapolated
+    dist : array_like
+           Stations distance
+    fd : float
+         Decay factor [0-1]
+
+
+    Returns
+    -------
+    Sec_extrap : array_like
+                 Extrapolated variable
+
+    """
+    from scipy.interpolate import interp1d
+
+    new_data1 = []
+    for row in data:
+        mask = ~np.isnan(row)
+        if mask.any():
+            y = row[mask]
+            if y.size == 1:
+                row = np.repeat(y, len(mask))
+            else:
+                x = dist[mask]
+                f_i = interp1d(x, y)
+                f_x = _extrap1d(f_i)
+                row = f_x(dist)
+        new_data1.append(row)
+
+    new_data2 = []
+    for col in data.T:
+        mask = ~np.isnan(col)
+        if mask.any():
+            y = col[mask]
+            if y.size == 1:
+                col = np.repeat(y, len(mask))
+            else:
+                z = depth[mask]
+                f_i = interp1d(z, y)
+                f_z = _extrap1d(f_i)
+                col = f_z(depth)
+        new_data2.append(col)
+
+    new_data = np.array(new_data1) * w1 + np.array(new_data2).T * w2
+    return new_data
+
+
+def gen_topomask(h, lon, lat, dx=1.0, kind="linear", plot=False):
+    """
+    Generates a topography mask from an oceanographic transect taking the
+    deepest CTD scan as the depth of each station.
+
+    Inputs
+    ------
+    h : array
+        Pressure of the deepest CTD scan for each station [dbar].
+    lons : array
+           Longitude of each station [decimal degrees east].
+    lat : Latitude of each station. [decimal degrees north].
+    dx : float
+         Horizontal resolution of the output arrays [km].
+    kind : string, optional
+           Type of the interpolation to be performed.
+           See scipy.interpolate.interp1d documentation for details.
+    plot : bool
+           Whether to plot mask for visualization.
+
+    Outputs
+    -------
+    xm : array
+         Horizontal distances [km].
+    hm : array
+         Local depth [m].
+
+    Author
+    ------
+    André Palóczy Filho (paloczy@gmail.com) --  October/2012
+
+    """
+
+    import gsw
+    from scipy.interpolate import interp1d
+
+    h, lon, lat = list(map(np.asanyarray, (h, lon, lat)))
+    # Distance in km.
+    x = np.append(0, np.cumsum(gsw.distance(lon, lat)[0] / 1e3))
+    h = -gsw.z_from_p(h, lat.mean())
+    Ih = interp1d(x, h, kind=kind, bounds_error=False, fill_value=h[-1])
+    xm = np.arange(0, x.max() + dx, dx)
+    hm = Ih(xm)
+
+    return xm, hm
+
+
+def plot_section(self, reverse=False, filled=False, **kw):
+    import gsw
+
+    lon, lat, data = list(map(np.asanyarray, (self.lon, self.lat, self.values)))
+    data = ma.masked_invalid(data)
+    h = self.get_maxdepth()
+    if reverse:
+        lon = lon[::-1]
+        lat = lat[::-1]
+        data = data.T[::-1].T
+        h = h[::-1]
+    lon, lat = map(np.atleast_2d, (lon, lat))
+    x = np.append(0, np.cumsum(gsw.distance(lon, lat)[0] / 1e3))
+    z = self.index.values.astype(float)
+
+    if filled:  # CAVEAT: this method cause discontinuities.
+        data = data.filled(fill_value=np.nan)
+        data = extrap_sec(data, x, z, w1=0.97, w2=0.03)
+
+    # Contour key words.
+    extend = kw.pop("extend", "both")
+    fontsize = kw.pop("fontsize", 12)
+    labelsize = kw.pop("labelsize", 11)
+    cmap = kw.pop("cmap", plt.cm.rainbow)
+    levels = kw.pop(
+        "levels", np.arange(np.floor(data.min()), np.ceil(data.max()) + 0.5, 0.5),
+    )
+
+    # Colorbar key words.
+    pad = kw.pop("pad", 0.04)
+    aspect = kw.pop("aspect", 40)
+    shrink = kw.pop("shrink", 0.9)
+    fraction = kw.pop("fraction", 0.05)
+
+    # Topography mask key words.
+    dx = kw.pop("dx", 1.0)
+    kind = kw.pop("kind", "linear")
+    linewidth = kw.pop("linewidth", 1.5)
+
+    # Station symbols key words.
+    station_marker = kw.pop("station_marker", None)
+    color = kw.pop("color", "k")
+    offset = kw.pop("offset", -5)
+    alpha = kw.pop("alpha", 0.5)
+
+    # Figure.
+    figsize = kw.pop("figsize", (12, 6))
+    fig, ax = plt.subplots(figsize=figsize)
+    xm, hm = gen_topomask(h, lon, lat, dx=dx, kind=kind)
+    ax.plot(xm, hm, color="black", linewidth=linewidth, zorder=3)
+    ax.fill_between(xm, hm, y2=hm.max(), color="0.9", zorder=3)
+
+    if station_marker:
+        ax.plot(
+            x,
+            [offset] * len(h),
+            color=color,
+            marker=station_marker,
+            alpha=alpha,
+            zorder=5,
+        )
+    ax.set_xlabel("Cross-shore distance [km]", fontsize=fontsize)
+    ax.set_ylabel("Depth [m]", fontsize=fontsize)
+    ax.set_ylim(offset, hm.max())
+    ax.invert_yaxis()
+
+    ax.xaxis.set_ticks_position("top")
+    ax.xaxis.set_label_position("top")
+    ax.yaxis.set_ticks_position("left")
+    ax.yaxis.set_label_position("left")
+    ax.xaxis.set_tick_params(tickdir="out", labelsize=labelsize, pad=1)
+    ax.yaxis.set_tick_params(tickdir="out", labelsize=labelsize, pad=1)
+
+    # Color version.
+    cs = ax.contourf(
+        x, z, data, cmap=cmap, levels=levels, alpha=1.0, extend=extend, zorder=2,
+    )  # manual=True
+    # Colorbar.
+    cb = fig.colorbar(
+        mappable=cs,
+        ax=ax,
+        orientation="vertical",
+        aspect=aspect,
+        shrink=shrink,
+        fraction=fraction,
+        pad=pad,
+    )
+    return fig, ax, cb
+
+
+def cell_thermal_mass(temperature, conductivity):
+    """
+    Sample interval is measured in seconds.
+    Temperature in degrees.
+    CTM is calculated in S/m.
+
+    """
+
+    alpha = 0.03  # Thermal anomaly amplitude.
+    beta = 1.0 / 7  # Thermal anomaly time constant (1/beta).
+
+    sample_interval = 1 / 15.0
+    a = 2 * alpha / (sample_interval * beta + 2)
+    b = 1 - (2 * a / alpha)
+    dCodT = 0.1 * (1 + 0.006 * [temperature - 20])
+    dT = np.diff(temperature)
+    ctm = -1.0 * b * conductivity + a * (dCodT) * dT  # [S/m]
+    return ctm
+
+
+def mixed_layer_depth(CT, method="half degree"):
+    if method == "half degree":
+        mask = CT[0] - CT < 0.5
+    else:
+        mask = np.zeros_like(CT)
+    return Series(mask, index=CT.index, name="MLD")
+
+
+def barrier_layer_thickness(SA, CT):
+    """
+    Compute the thickness of water separating the mixed surface layer from the
+    thermocline.  A more precise definition would be the difference between
+    mixed layer depth (MLD) calculated from temperature minus the mixed layer
+    depth calculated using density.
+
+    """
+    import gsw
+
+    sigma_theta = gsw.sigma0(SA, CT)
+    mask = mixed_layer_depth(CT)
+    mld = np.where(mask)[0][-1]
+    sig_surface = sigma_theta[0]
+    sig_bottom_mld = gsw.sigma0(SA[0], CT[mld])
+    d_sig_t = sig_surface - sig_bottom_mld
+    d_sig = sigma_theta - sig_bottom_mld
+    mask = d_sig < d_sig_t  # Barrier layer.
+    return Series(mask, index=SA.index, name="BLT")
+
+
+def get_distance(lat1, lon1, lat2, lon2):
+
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
+
+
+def speed_test(metadata, cfg):
+    """Compute speed between stations.
+        
+        A too short time between stations may indicate a problem in
+        geoposition. Typically, a research vessel does not exceed 15 knots. 
+    """
+    cfg = cfg["main"]["max_speed"]
+
+    lats = [d["lat"] for d in metadata]
+    lons = [d["lon"] for d in metadata]
+    times = [d["time"] for d in metadata]
+    stations = [d["station"] for d in metadata]
+    lats = [x for _, x in sorted(zip(times, lats), key=lambda x: x[0])]
+    lons = [x for _, x in sorted(zip(times, lons), key=lambda x: x[0])]
+    stations = [x for _, x in sorted(zip(times, stations), key=lambda x: x[0])]
+
+    for idx in range(len(times) - 1):
+        distance = get_distance(lats[idx], lons[idx], lats[idx + 1], lons[idx + 1])
+        delta_time = (times[idx + 1] - times[idx]).total_seconds()
+        speed = (distance / 1.852) / (delta_time / 3600)
+        if speed > cfg:
+            print(
+                "WARNING: Navigation too fast between "
+                + stations[idx]
+                + " and "
+                + stations[idx + 1]
+            )
+            print("         Please, check positions!")
+    return
---
+++ formatting.py
@@ -0,0 +1,453 @@
+import csv
+from PIL import Image
+from fpdf import FPDF
+import json
+import pkg_resources
+import math
+from pathlib import Path
+import numpy as np
+
+
+def to_cnv_flagged(df, metadata, output_file):
+    """ Write a taylor-made cnv file, formatted to facilitate the input in 
+       NEMO software (SeaDataNet). Column names follow SeaDataNet convention
+       and FLAGS are included.
+       
+       Input: - Dataframe obtained from a previous read with 'from_cnv'
+              - metadata info as obatined with 'from_cnv'
+              - output file including the route
+    """
+
+    for column in df.columns:
+        if column != "FLAGS":
+            totalnum = []
+            decimals = []
+            kk = df[column].to_string(index=False).replace(" ", "").split("\n")
+            for element in kk:
+                integral, fractional = (
+                    element.split(".") if "." in element else (element, "")
+                )
+                totalnum.append(len(element))
+                decimals.append(len(fractional))
+            maxprec = max(decimals)
+            # Output from SeaBird has 11 characters for each variable
+            # df_formatted[i] = df[i].map(lambda x: '{:>{width}.{prec}f}'.format(x, width=maxwidth, prec=maxprec) )
+            # df[column] = df[column].map(lambda x: '{:>{width}.{prec}f}'.format(x, width=11, prec=maxprec) )
+            df[column] = df[column].map(lambda x: "{:.{prec}f}".format(x, prec=maxprec))
+
+    if "FLAGS" in df.columns:
+        # df.iloc[:,-1] = df.iloc[:,-1].map(lambda x: '{:>11}'.format(x.replace(" ", "")) )
+        df.iloc[:, -1] = df.iloc[:, -1].map(lambda x: "{:}".format(x.replace(" ", "")))
+
+    # Use it if you want to insert spaces in the first column
+    # df_formatted.insert(0, 'tabulation', '    ')
+
+    with open(output_file, "w", encoding="utf8", newline="") as outfile:
+        # Insert configuration info before last row
+        header = metadata["header"]
+        outfile.write(header[: header.rfind("\n")] + "\n")
+        outfile.write(metadata["config"] + "\n")
+        outfile.write("*END*\n")
+        # df.to_csv(outfile, sep='\t', header=False, index=False, quoting=csv.QUOTE_NONE)
+        df.to_csv(outfile, header=False, index=False, quoting=csv.QUOTE_NONE)
+    return
+
+
+def to_cnv_medatlas(df, metadata, output_file):
+    """ Write a *.dat file, formatted following MEDATLAS format and SeaDataNet
+        conventions
+       
+       Input: - Dataframe obtained from a previous read with 'from_cnv'
+              - metadata info as obatined with 'from_cnv'
+              - output file including the route
+    """
+
+    accuracy = json.loads(pkg_resources.resource_string("ctd", "accuracy.json"))
+
+
+    # Find missing values in original data previous to any reformat
+    lines=metadata['config']
+    matched_lines = [line for line in lines.split('\n') if "bad_flag" in line]    
+    if matched_lines:
+        bad_flag = float(matched_lines[0].split("=")[1].strip())
+        df = df.replace(bad_flag, np.nan)
+        
+
+    # Format values
+    for column in df.columns:
+        if column != "FLAGS":
+
+            scale = accuracy[column]["decimals"]
+
+            if column == "PRES":
+                precision = accuracy[column]["digits"]
+                width = min(len(str(df[column].max()).strip()), precision)
+                df[column] = df[column].map(
+                    lambda x: "{:>{width}.{prec}f}".format(
+                        x, width=width + 1, prec=scale
+                    )
+                )
+            else:
+
+                totalint = []
+                decimals = []
+                kk = df.loc[df[column].notnull(), column].to_string(index=False).replace(" ", "").split("\n")
+                for element in kk:
+                    integral, fractional = (
+                        element.split(".") if "." in element else (element, "")
+                    )
+                    totalint.append(len(integral))
+                    decimals.append(len(fractional))
+
+                scale = min(max(decimals), scale)
+                width = max(totalint) + scale + 1
+                #df[column][df[column].notnull()] = df[column][df[column].notnull()].map(
+                #    lambda x: "{:>{width}.{prec}f}".format(x, width=width, prec=scale)
+                #)
+                df.loc[df[column].notnull(), column] = df.loc[df[column].notnull(), column].map(
+                    lambda x: "{:>{width}.{prec}f}".format(x, width=width, prec=scale)
+                )
+                
+    # Add default values at the end. First append a new row
+    width = len(str(df["PRES"].iloc[-1]))
+    new_row = {"PRES": "{:.{width}}".format("-9999", width=width)}
+    df = df.append(new_row, ignore_index=True)
+
+    for column in df.columns:
+        if column != "PRES":        
+            #last_value = df[column].iloc[-2]
+            #last_value = max(df[column][:-2])
+            last_value = max(df[column][df[column].notnull()])
+            number = last_value.strip().split(".") if "." in element else (element, "")
+            integral = len(number[0])
+            if len(number) == 1:
+                df.iloc[-1, df.columns.get_loc(column)] = "{:.{width}}".format(
+                    "99999999999", width=integral
+                )
+            else:
+                fractional = len(number[1])
+                df.iloc[-1, df.columns.get_loc(column)] = (
+                    "{:.{width}}".format("99999999999", width=integral)
+                    + "."
+                    + "{:.{width}}".format("999999", width=fractional)
+                )
+
+   
+    if matched_lines:
+        for column_index, variable in enumerate(df.keys()):
+            # Change flag to 9 is a NaN is present
+            s = list(df['FLAGS'][df[variable].isnull()])
+            new_s = []
+            for idx, element in enumerate(s):
+                numbers_in_flag = list(element)
+                numbers_in_flag[column_index] = '9'
+                new_s.append(''.join(numbers_in_flag))
+            df['FLAGS'][df[variable].isnull()] = new_s
+            # And modify the NaN value for MedAtlas format (last line)
+            df[variable] = df[variable].fillna(df.iloc[-1][variable])
+        
+    
+    
+    # Use it if you want to insert spaces in the first column
+    # df_formatted.insert(0, 'tabulation', '    ')
+
+    # output_file=fname
+    cruise_id = output_file.name[4:17]
+    # station_id = cruise_id + metadata['name'].zfill(5)
+    station_id = cruise_id + metadata["station"].zfill(5)
+    station_date = metadata["time"].strftime("%d%m%Y")
+    station_time = metadata["time"].strftime("%H%M")
+    local_cdi_id = cruise_id + "_" + metadata["station"].zfill(5) + "_H10"
+
+    lat = math.modf(metadata["lat"])
+    if lat[1] > 0:
+        station_lat = (
+            "N"
+            + str(int(lat[1])).zfill(2)
+            + " "
+            + "{:02.{prec}f}".format(abs(lat[0]) * 60, prec=2).zfill(5)
+        )
+    else:
+        station_lat = (
+            "S"
+            + str(abs(int(lat[1]))).zfill(2)
+            + " "
+            + "{:02.{prec}f}".format(abs(lat[0]) * 60, prec=2).zfill(5)
+        )
+
+    lon = math.modf(metadata["lon"])
+    if lon[1] > 0:
+        station_lon = (
+            "E"
+            + str(int(lon[1])).zfill(3)
+            + " "
+            + "{:02.{prec}f}".format(abs(lon[0]) * 60, prec=2).zfill(5)
+        )
+    else:
+        station_lon = (
+            "W"
+            + str(abs(int(lon[1]))).zfill(3)
+            + " "
+            + "{:02.{prec}f}".format(abs(lon[0]) * 60, prec=2).zfill(5)
+        )
+
+    # Compute Global Parameter Quality Flag following Maillard et al.
+    flagglobal = ""
+    for idx in range(len(str(df["FLAGS"][0]))):
+        flagparam = []
+        for element in df["FLAGS"][0:-1]:
+            flagparam.append(element[idx])
+        if flagparam.count("4") / len(flagparam) > 0.2:
+            flagglobal = flagglobal + "4"
+        elif (
+            (flagparam.count("4") / len(flagparam))
+            + (flagparam.count("3") / len(flagparam))
+        ) > 0.2:
+            flagglobal = flagglobal + "3"
+        elif (
+            (flagparam.count("4") / len(flagparam))
+            + (flagparam.count("3") / len(flagparam))
+            + (flagparam.count("2") / len(flagparam))
+        ) > 0.2:
+            flagglobal = flagglobal + "2"
+        else:
+            flagglobal = flagglobal + "1"
+
+    # Build a header (note that depth is not mandatory)
+    header = ""
+    header = "*" + station_id + " Data Type=H10\n"
+    if metadata["depth"] is None:
+        header = (
+            header
+            + "*DATE="
+            + station_date
+            + " TIME="
+            + station_time
+            + " LAT="
+            + station_lat
+            + " LON="
+            + station_lon
+            + " DEPTH=       QC=1119"
+            + "\n"
+        )
+    else:
+        header = (
+            header
+            + "*DATE="
+            + station_date
+            + " TIME="
+            + station_time
+            + " LAT="
+            + station_lat
+            + " LON="
+            + station_lon
+            + " DEPTH="
+            + str(metadata["depth"]).ljust(7)
+            + "QC=1111"
+            + "\n"
+        )
+    header = (
+        header
+        + "*NB PARAMETERS="
+        + str(len(df.columns) - 1).zfill(2)
+        + " RECORD LINES="
+        + str(len(df) - 1).zfill(5)
+        + "\n"
+    )
+    for column in df.columns:
+        if column == "PRES":
+            header = (
+                header
+                + "*PRES SEA PRESSURE sea surface=0    (decibar=10000 pascals       ) def.="
+                + str(df["PRES"].iloc[-1])
+                + "\n"
+            )
+        if column == "TEMP":
+            header = (
+                header
+                + "*TEMP SEA TEMPERATURE               (Celsius degree              ) def.="
+                + str(df["TEMP"].iloc[-1])
+                + "\n"
+            )
+        if column == "PSAL":
+            header = (
+                header
+                + "*PSAL PRACTICAL SALINITY            (P.S.U.                      ) def.="
+                + str(df["PSAL"].iloc[-1])
+                + "\n"
+            )
+        if column == "CNDC":
+            header = (
+                header
+                + "*CNDC ELECTRICAL CONDUCTIVITY       (mho/meter                   ) def.="
+                + str(df["CNDC"].iloc[-1])
+                + "\n"
+            )
+        if column == "TUR4":
+            header = (
+                header
+                + "*TUR4 TURBIDITY                     (N.T.U Nephelo Turb. Unit    ) def.="
+                + str(df["TUR4"].iloc[-1])
+                + "\n"
+            )
+        if column == "LGHT":
+            header = (
+                header
+                + "*LGHT LIGHT IRRADIANCE IMMERGED PAR (micromole photon/(m2.second)) def.="
+                + str(df["LGHT"].iloc[-1])
+                + "\n"
+            )
+        if column == "OSAT":
+            header = (
+                header
+                + "*OSAT OXYGEN SATURATION             (%                           ) def.="
+                + str(df["OSAT"].iloc[-1])
+                + "\n"
+            )
+        if column == "FLU2":
+            header = (
+                header
+                + "*FLU2 FLUORESCENCE                  (milligram/m3                ) def.="
+                + str(df["FLU2"].iloc[-1])
+                + "\n"
+            )
+    header = (
+        header
+        + "*GLOBAL PROFILE QUALITY FLAG=1 GLOBAL PARAMETERS QC FLAGS="
+        + flagglobal
+        + "\n"
+    )
+    header = (
+        header
+        + "*DC HISTORY=\n*\n*DM HISTORY=\n*\n*COMMENT\n*\n*SDN_parameter_mapping\n"
+    )
+    for column in df.columns:
+        if column == "PRES":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:PRES</subject><object>SDN:P01::PRESPR01</object><units>SDN:P06::UPDB</units>"
+                + "\n"
+            )
+        if column == "TEMP":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:TEMP</subject><object>SDN:P01::TEMPPR01</object><units>SDN:P06::UPAA</units>"
+                + "\n"
+            )
+        if column == "PSAL":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:PSAL</subject><object>SDN:P01::PSLTZZ01</object><units>SDN:P06::UUUU</units>"
+                + "\n"
+            )
+        if column == "CNDC":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:CNDC</subject><object>SDN:P01::CNDCZZ01</object><units>SDN:P06::UECA</units>"
+                + "\n"
+            )
+        if column == "TUR4":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:TUR4</subject><object>SDN:P01::TURBXXXX</object><units>SDN:P06::USTU</units>"
+                + "\n"
+            )
+        if column == "LGHT":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:LGHT</subject><object>SDN:P01::PARERXUD</object><units>SDN:P06::UMES</units>"
+                + "\n"
+            )
+        if column == "OSAT":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:OSAT</subject><object>SDN:P01::OXYSZZ01</object><units>SDN:P06::UPCT</units>"
+                + "\n"
+            )
+        if column == "FLU2":
+            header = (
+                header
+                + "*<subject>SDN:LOCAL:FLU2</subject><object>SDN:P01::CPHLPM01</object><units>SDN:P06::UMMC</units>"
+                + "\n"
+            )
+
+    header = header + "*EDMO_CODE=353" + "\n"
+    header = header + "*LOCAL_CDI_ID=" + local_cdi_id + "\n"
+    header = (
+        header
+        + '*<sdn_reference xlink:href="http://seadata.bsh.de/cgi-csr/XML/xmlDownload_V2.pl?edmo=353&identifier='
+        + cruise_id
+        + '" xlink:role="isObservedBy" xlink:type="SDN:L23::CSR"/>'
+        + "\n"
+    )
+    header = (
+        header
+        + '*<sdn_reference xlink:href="http://vocab.nerc.ac.uk/collection/C17/current/'
+        + cruise_id[0:4]
+        + '" xlink:role="isObservedBy" xlink:type="SDN:L23::NVS2CON"/>'
+        + "\n"
+    )
+    header = (
+        header
+        + '*<sdn_reference xlink:href="http://seadatanet.maris2.nl/v_cdi_v3/print_xml.asp?edmo=353&identifier='
+        + local_cdi_id
+        + '" xlink:role="isDescribedBy" xlink:type="SDN:L23::CDI"/>'
+        + "\n"
+    )
+    header = header + "*SURFACE SAMPLES=\n*\n"
+    header = header + "*"
+    for column in df.columns:
+        if column != "FLAGS":
+            header = header + column + "  "
+
+    with open(output_file, "a", encoding="utf8", newline="") as outfile:
+        outfile.write(header + "\n")
+        # to_csv does not allow to use space character as delimiter
+        for index, rows in df.iterrows():
+            outfile.write(" ".join(rows.values) + "\n")
+
+    return
+
+
+def imglist_to_pdf(imglist, pdf_file):
+
+    pdf = FPDF()
+
+    for image in imglist:
+
+        cover = Image.open(image)
+        width, height = cover.size
+
+        # convert pixel in mm with 1px=0.264583 mm
+        width, height = float(width * 0.264583), float(height * 0.264583)
+
+        # given we are working with A4 format size
+        pdf_size = {"P": {"w": 210, "h": 297}, "L": {"w": 297, "h": 210}}
+
+        # get page orientation from image size
+        orientation = "P" if width < height else "L"
+
+        #  make sure image size is not greater than the pdf format size
+        width = (
+            width if width < pdf_size[orientation]["w"] else pdf_size[orientation]["w"]
+        )
+        height = (
+            height
+            if height < pdf_size[orientation]["h"]
+            else pdf_size[orientation]["h"]
+        )
+        if width == height:
+            orientation = "P"
+        pdf.add_page(orientation=orientation)
+
+        align_center = True
+        if align_center == True:
+            left = (pdf_size[orientation]["w"] - width) / 2
+            top = (pdf_size[orientation]["h"] - height) / 2
+        else:
+            left = 0
+            top = 0
+
+        pdf.image(image, left, top, width, height)
+
+    pdf.output(pdf_file, "F")
